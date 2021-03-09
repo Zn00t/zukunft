31b48b9652dc2ce4473e760cd6608288222e80d3
Fabi <fabian.boehm99@yahoo.de>
2021-03-09 13:16:33 +0100

    FinanceValues Table.sum is now added up from food, invest, cleaning

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,13 @@
 class HomeController < ApplicationController
+
+  before_action :calcsum
+
   def index
     @value = FinanceValue.all
     @user = User.all
+  end
+
+  def calcsum
+    FinanceValue.update_all "sum = food + invest + cleaning"
   end
 end

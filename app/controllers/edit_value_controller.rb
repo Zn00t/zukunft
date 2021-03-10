fcc27f2f0bc30e5d47e71ffff1a79978010e9c7e
Fabi <fabian.boehm99@yahoo.de>
2021-03-10 19:45:17 +0100

    added Buttons in table

---
+++ app/controllers/edit_value_controller.rb
@@ -0,0 +1,10 @@
+class EditValueController < ApplicationController
+
+  def edit
+
+  end
+
+  def update
+
+  end
+end
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,5 @@
 const { environment } = require('@rails/webpacker')
 
+environment.loaders.delete("node_modules")
+
 module.exports = environment

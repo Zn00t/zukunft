f99619e95e1e3532cfed7cb8164e0cb6846ff81f
Fabi <fabian.boehm99@yahoo.de>
2021-04-03 18:39:05 +0200

    fixed restack creation statement

--- app/controllers/edit_value_controller.rb
+++ app/controllers/edit_value_controller.rb
@@ -18,7 +18,7 @@ class EditValueController < ApplicationController
   end
   def upload_restack
     if params[:value] != "" &&
-      Restack.create(from_id: Current.user.id, to_id: params[:id], value: params[:value], user_id: Current.user.id prompted: false)
+      Restack.create(from_id: Current.user.id, to_id: params[:id], value: params[:value], user_id: Current.user.id, prompted: false)
       redirect_to root_path, notice: "Umschichtung vorgeschlagen."
     else
       redirect_to restack_path, alert: "Bitte gib einen Wert ein."

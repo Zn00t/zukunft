b488c43aa72fbbea8f98365e6df7676f723fc700
boehm1 <gitgud123>
2021-12-14 11:23:32 +0100

    changed :int to :integer

--- app/models/finance_value.rb
+++ app/models/finance_value.rb
@@ -2,11 +2,8 @@ class FinanceValue < ApplicationRecord
   belongs_to :user
 
   attribute :sum, :float, default: 0
-  attribute :rate, :int, default: 25
+  attribute :rate, :integer, default: 25
   attribute :food, :float, default: 0
   attribute :invest, :float, default: 0
-  attribute :cleaning, :int, default: 0
-
-  attribute :cleaning, :int, default: 0
-
+  attribute :cleaning, :integer, default: 0
 end

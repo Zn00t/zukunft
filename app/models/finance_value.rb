07d90f36cebed6aacf19cd5477c2b8f7c414d7f9
boehm1 <gitgud123>
2021-12-14 11:13:17 +0100

    fixed db user creation

--- app/models/finance_value.rb
+++ app/models/finance_value.rb
@@ -1,3 +1,12 @@
 class FinanceValue < ApplicationRecord
   belongs_to :user
+
+  attribute :sum, :float, default: 0
+  attribute :rate, :int, default: 25
+  attribute :food, :float, default: 0
+  attribute :invest, :float, default: 0
+  attribute :cleaning, :int, default: 0
+
+  attribute :cleaning, :int, default: 0
+
 end

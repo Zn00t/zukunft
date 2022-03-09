bce0982fa8108d1ede146c7d25ee1759ec3fd4ea
zn00t <gitgud123>
2022-03-09 11:12:23 +0100

    fixed admincorner tasks

--- app/controllers/admincorner_controller.rb
+++ app/controllers/admincorner_controller.rb
@@ -10,23 +10,23 @@ class AdmincornerController < ApplicationController
   end
 
   def change
-    if params["/admincorner"][:method] == "activate"
-      User.update(params["/admincorner"][:id], :active => true)
-      redirect_to admincorner_path, notice: "#{User.find(params["/admincorner"][:id]).name} #{t('alertWasActivated')}"
+    if params["/admincorner?locale=#{params[:locale]}"][:method] == "activate"
+      User.update(params["/admincorner?locale=#{params[:locale]}"][:id], :active => true)
+      redirect_to admincorner_path, notice: "#{User.find(params["/admincorner?locale=#{params[:locale]}"][:id]).name} #{t('alertWasActivated')}"
     end
-    if params["/admincorner"][:method] == "deactivate"
-      User.update(params["/admincorner"][:id], :active => false)
-      redirect_to admincorner_path, notice: "#{User.find(params["/admincorner"][:id]).name} #{t('alertWasDeactivated')}"
+    if params["/admincorner?locale=#{params[:locale]}"][:method] == "deactivate"
+      User.update(params["/admincorner?locale=#{params[:locale]}"][:id], :active => false)
+      redirect_to admincorner_path, notice: "#{User.find(params["/admincorner?locale=#{params[:locale]}"][:id]).name} #{t('alertWasDeactivated')}"
     end
-    if params["/admincorner"][:method] == "add_user"
-      if User.find_by_name(params["/admincorner"][:name])
+    if params["/admincorner?locale=#{params[:locale]}"][:method] == "add_user"
+      if User.find_by_name(params["/admincorner?locale=#{params[:locale]}"][:name])
         redirect_to admincorner_path, alert: "#{t('alertUserAlreadyExists')}" and return
       end
-      User.create(:name => params["/admincorner"][:name], :password => "zukunft", :password_confirmation => "zukunft", :admin => false, :active => true)
-      redirect_to admincorner_path, notice: "#{User.find_by_name(params["/admincorner"][:name]).name} #{t('alertWasAdded')}"
+      User.create(:name => params["/admincorner?locale=#{params[:locale]}"][:name], :password => "zukunft", :password_confirmation => "zukunft", :admin => false, :active => true)
+      redirect_to admincorner_path, notice: "#{User.find_by_name(params["/admincorner?locale=#{params[:locale]}"][:name]).name} #{t('alertWasAdded')}"
     end
-    if params["/admincorner"][:method] == "admin_checkbox"
-      User.update(params["/admincorner"][:id], :admin => params["/admincorner"][:admin])
+    if params["/admincorner?locale=#{params[:locale]}"][:method] == "admin_checkbox"
+      User.update(params["/admincorner?locale=#{params[:locale]}"][:id], :admin => params["/admincorner?locale=#{params[:locale]}"][:admin])
       redirect_to admincorner_path, notice: t('alertUpdatedPermissions') and return
     end
   end

3d3d29d11a6f67cc85d4f32e349ba7e05ac7f07d
Jan Steinmetz <jan@kopfmaschine.com>
2022-11-26 10:12:08 +0100

    some more capistrano setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :passenger_restart_with_touch, true
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml", 'config/master.key'
+append :linked_files, 'config/master.key'
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"

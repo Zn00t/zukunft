6716fd6331132df67c1a7a8c183aecc62e457525
Jan Steinmetz <jan@kopfmaschine.com>
2024-01-14 16:56:06 +0100

    fix: adding username to debtmessage

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.17.1"
+lock "~> 3.18"
 
 set :application, "Zukunftfinances"
 set :repo_url, "git@github.com:Zn00t/zukunft.git"

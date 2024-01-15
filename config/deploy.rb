eafc7c8906195d63c2ba11aa4effc52b10a5cb3a
Jan Steinmetz <jan@kopfmaschine.com>
2024-01-15 18:10:31 +0100

    fix: share storage and node module dir between releases

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,6 +30,7 @@ append :linked_files, 'config/master.key'
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
+append :linked_dirs, "log", "storage", "public", "node_modules"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }

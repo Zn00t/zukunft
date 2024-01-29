93f26ec541bcd9fe8f5dfcdf1ed8655ab36b31b2
Jan Steinmetz <jan@kopfmaschine.com>
2024-01-29 11:00:59 +0100

    debtnotification: add sum to plus users

--- lib/notifications.rb
+++ lib/notifications.rb
@@ -7,7 +7,7 @@ module Notifications
     debt_users = debt_users.map{|u| chat.user_mention(u.name, u.telegram_id)}
 
     plus_users = User.active.select(:id, :name, :telegram_id, "(food + invest + cleaning) AS sum").order(sum: :asc).limit(3).all
-    plus_users = plus_users.map{|u| chat.user_mention(u.name, u.telegram_id)}
+    plus_users = plus_users.map{|u| "#{chat.user_mention(u.name, u.telegram_id)} (#{u[:sum]} €)"}
     chat.send_message("#{debt_users.join(' ')} haben mehr als 100euro schulden.\nBitte sendet etwas an #{plus_users.join(' ')}. Dankee!")
   end
 

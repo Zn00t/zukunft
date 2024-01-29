a3aab3f96cd3005e6314595c03bcbec185d52d38
Jan Steinmetz <jan@kopfmaschine.com>
2024-01-29 10:57:08 +0100

    change ordering for debtmessages

--- lib/notifications.rb
+++ lib/notifications.rb
@@ -6,7 +6,7 @@ module Notifications
     debt_users = User.active.where('food + cleaning + invest > ?', 100).all
     debt_users = debt_users.map{|u| chat.user_mention(u.name, u.telegram_id)}
 
-    plus_users = User.active.select(:id, :name, :telegram_id, "(food + invest + cleaning) AS sum").order(sum: :desc).limit(3).all
+    plus_users = User.active.select(:id, :name, :telegram_id, "(food + invest + cleaning) AS sum").order(sum: :asc).limit(3).all
     plus_users = plus_users.map{|u| chat.user_mention(u.name, u.telegram_id)}
     chat.send_message("#{debt_users.join(' ')} haben mehr als 100euro schulden.\nBitte sendet etwas an #{plus_users.join(' ')}. Dankee!")
   end
